5e25d5e81532ff09f76212761d6bf4eaf4e51456
talitarp <162145753+talitarp@users.noreply.github.com>
2024-03-26 11:49:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,40 @@
 from kytos.core import KytosNApp, log, rest
 from kytos.core import KytosEvent
 from kytos.core.helpers import listen_to
-from kytos.core.rest_api import (HTTPException, JSONResponse, Request, get_json_or_400)  
+from kytos.core.rest_api import (HTTPException, JSONResponse, Request,
+                                 get_json_or_400)  
 
 class Main(KytosNApp):
     """Main class of talitarp/contention_block NApp.This class is the entry point for this napp."""  
     
      """ Handle the LOCK contention event"""
 
+    def setup(self):
+        """Replace the '__init__' method for the KytosNApp subclass.
+
+        The setup method is automatically called by the controller when your
+        application is loaded.
+
+        """
+        log.info("Starting Kytos SDNTrace CP App!")
+
+    def execute(self):
+        """This method is executed right after the setup method execution.
+
+        You can also use this method in loop mode if you add to the above setup
+        method a line like the following example:
+
+            self.execute_as_loop(30)  # 30-second interval.
+        """
+
+    def shutdown(self):
+        """This method is executed when your napp is unloaded.
+
+        If you have some cleanup procedure, insert it here.
+        """
+        
      @rest('/v1/contention_block/', methods=['POST'])
-     def contention_block(self, request):
+     def contention_block(self, request: Request) -> JSONResponse:
          data=get_json_or_400(request,self.controller.loop)
             print "==> contention_block ipaddr=%s in all switches" % (ipaddr)
             actions = []
