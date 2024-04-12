2edbba563bd5c26b10eaf22c0c308bb46d5e465e
Italo Valcy <italo@ampath.net>
2024-04-12 18:00:57 -0300

    adding basic functions to block a traffic

--- main.py
+++ main.py
@@ -42,25 +42,63 @@ class Main(KytosNApp):
 
     def validate_input(self, data):
         # TODO: validate all user inputs
-        return True
+        mandatory_fields = ["switch", "interface", "match"]
+        # check switch
+        switch_id = data.get("switch")
+        if not switch_id or switch_id not in self.controller.switches:
+            return False, f"Invalid switch: {switch_id}"
+        switch = self.controller.switches[switch_id]
+
+        # check interface (port_no 1)
+        try:
+            port_no = data.get("interface", "")
+            port_no = int(port_no)
+        except:
+            return False, f"Invalid interface: {port_no}"
+        if port_no not in switch.interfaces:
+            return False, f"Unknown interface: {port_no}"
+        interface = switch.interfaces[port_no]
+
+        # check matching fields
+        match = data.get("match")
+        if not match:
+            return False, f"Invalid match: {match}"
+        if "vlan" not in match:
+            return False, "Missing mandatory field vlan on match"
+
+        expected_fields = ["ipv4_src", "ipv4_dst", "ipv6_src", "ipv6_dst", "ip_proto", "sport", "dport", "vlan"]
+        for key in match:
+            if key not in expected_fields:
+                return False, f"Unexpected input match field: {key}"
+
+        return True, "success"
 
     @rest('/v1/contention_block', methods=['POST'])
     def contention_block(self, request: Request) -> JSONResponse:
         data = get_json_or_400(request, self.controller.loop) #access user request
-        if not self.validate_input(data):
-            raise HTTPException(400, "Invalid request data")
+        result, msg = self.validate_input(data)
+        if not result:
+            raise HTTPException(400, f"Invalid request data: {msg}")
         log.info(f"contention_block called with data={data}")
+
+        # Call flow_manager's REST API to create the flow
+        #payload = {"flows": [{"priority": 30000, "hard_timeout": xxx, "cookie": 0xee00000000000001, "match": {"in_port": xxx, "dl_vlan": xxx, "nw_src": xxx, "nw_dst": xxx, "nw_proto": xxx}, "actions": []}]}
+        payload = {"flows": [{"priority": 30000, "cookie": 0xee00000000000001, "match": {"in_port": int(data["interface"]), "dl_vlan": data["match"]["vlan"]}, "actions": []}]}
+        dpid = data["switch"]
+        response = requests.post(f"http://127.0.0.1:8181/api/kytos/flow_manager/v2/flows/{dpid}", json=payload)
+        if response.status_code != 202:
+            raise HTTPException(400, f"Invalid request to flow_manager: {response.text}")
 
         return JSONResponse({"result": "contentation created successfully"})
 
 
         # 1. descrever a API REST
         # quais argumentos vamos aceitar?
-        # - em qual switch vamos bloquear
+        # - em qual switch vamos bloquear (mandatory)
         # - em qual porta do switch vamos bloquear (podemos bloquear em mais de uma porta? ou melhor que seja numa porta especifica e caso queria bloquear em mais de uma porta vc mandaria multiplas requisicoes?)
-        #   -> o IDS vai ter uma base de mapeamento sobre onde o ataque foi visto e onde ele sera bloqueado (ou isso ficaria no proprio Kytos)
-        # - quais os criterios de bloqueio (match fields)
-        #   - VLAN
+        #   -> o IDS vai ter uma base de mapeamento sobre onde o ataque foi visto e onde ele sera bloqueado (ou isso ficaria no proprio Kytos) -- mandatory
+        # - quais os criterios de bloqueio (match fields) --  mandatory
+        #   - VLAN  -- mandatory
         #   - IP de origem (v4 e v6) -- aceita mascara 192.168.0.0/24
         #   - IP de destino (v4 e v6) -- aceita mascara
         #   - Protocolo IP (tcp/UDP/ICMP/IGMP,etc)
