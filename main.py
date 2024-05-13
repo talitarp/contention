bccf3649bd94bd72bba3ca749ffded33c9da5c6b
talitarp <162145753+talitarp@users.noreply.github.com>
2024-05-13 14:52:30 -0300

    Testing: Remove rule only ID block

--- main.py
+++ main.py
@@ -92,21 +92,22 @@ class Main(KytosNApp):
 
         if action == 'POST' or action == 'GET':
             payload = {"flows": [{"priority": 30000, "cookie": 0xee00000000000001, "match": {"in_port": int(data["interface"]), "dl_vlan": data["match"]["vlan"]}, "actions": []}]}
+        
+            if "ipv4_src" in data["match"]:
+                payload["flows"][0]["match"]["dl_type"] = 0x800
+                payload["flows"][0]["match"]["nw_src"] = data["match"]["ipv4_src"]
+            if "ipv4_dst" in data["match"]:
+                payload["flows"][0]["match"]["dl_type"] = 0x800
+                payload["flows"][0]["match"]["nw_dst"] = data["match"]["ipv4_dst"]
+            if "ip_proto" in data["match"]:
+                payload["flows"][0]["match"]["nw_proto"] = data["match"]["ip_proto"]
+            if "block_id" in data:
+                payload["flows"][0]["block_id"] = data["block_id"]
+
         if action == 'DELETE': 
             block_id = data.get("block_id")
             # payload = {"flows": [{"priority": 30000, "cookie": 0xee00000000000001, "cookie_mask": 0xffffffffffffffff, "match": {"in_port": int(data["interface"]), "dl_vlan": data["match"]["vlan"]}, "actions": []}]}
             payload = {"flows": [{"priority": 30000, "cookie": 0xee00000000000001, "cookie_mask": 0xffffffffffffffff, "match": {"in_port": int(self.stored_blocks["blocks"][block_id]["interface"]), "dl_vlan": self.stored_blocks["blocks"][block_id]["match"]["vlan"]}, "actions": []}]}
-       
-        if "ipv4_src" in data["match"]:
-            payload["flows"][0]["match"]["dl_type"] = 0x800
-            payload["flows"][0]["match"]["nw_src"] = data["match"]["ipv4_src"]
-        if "ipv4_dst" in data["match"]:
-            payload["flows"][0]["match"]["dl_type"] = 0x800
-            payload["flows"][0]["match"]["nw_dst"] = data["match"]["ipv4_dst"]
-        if "ip_proto" in data["match"]:
-            payload["flows"][0]["match"]["nw_proto"] = data["match"]["ip_proto"]
-        if "block_id" in data:
-            payload["flows"][0]["block_id"] = data["block_id"]
 		
         return payload
     
