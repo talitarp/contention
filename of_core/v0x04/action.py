729f8da4779134d297f3893b8faad611cd934eae
talitarp <162145753+talitarp@users.noreply.github.com>
2024-07-28 22:30:32 -0300

    Update action.py

--- of_core/v0x04/action.py
+++ of_core/v0x04/action.py
@@ -1,3 +1,20 @@
 """Of_core.v0x04.action"""
 
 from napps.kytos.of_core.flow import ActionBase
+
+class ActionSetIpv4dst(ActionBase):
+    """Action to send INT report."""
+
+    def __init__(self, ip):
+        self.ip = ip
+        self.action_type = "set_ipv4_dst"
+
+    @classmethod
+    def from_of_action(cls, of_action):
+    """Return a high-level ActionSetIpv4dst instance from pyof."""
+        return cls()
+
+    def as_of_action(self):
+    """Return a pyof ActionSetIpv4dst instance."""
+        return OFActionSetIpv4dst()
+
